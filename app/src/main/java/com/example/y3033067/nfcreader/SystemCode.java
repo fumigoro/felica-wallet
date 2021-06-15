07e5b57bcc76e5a4a09072b48a36a40252dffd55
FUMI <51395778+fumigoro@users.noreply.github.com>
2021-06-16 02:18:25 +0900

    add SystemCode.java

---
+++ app/src/main/java/com/example/y3033067/nfcreader/SystemCode.java
@@ -0,0 +1,8 @@
+package com.example.y3033067.nfcreader;
+
+public class SystemCode {
+    public static final int AYUCA = 0x83EE;
+    public static final int CAMPUS_PAY = 0x8E4B;
+    public static final int STUDENT_ID = 0x0447;
+
+}
