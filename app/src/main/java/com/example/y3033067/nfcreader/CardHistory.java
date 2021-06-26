01e2d32b5a93f38e221c2ca2d4c182cac421ddbe
FUMI <51395778+fumigoro@users.noreply.github.com>
2021-06-27 02:50:16 +0900

    ポイント関係を返すメソッドを3つ実装

--- app/src/main/java/com/example/y3033067/nfcreader/CardHistory.java
+++ app/src/main/java/com/example/y3033067/nfcreader/CardHistory.java
@@ -5,23 +5,25 @@ import java.util.Date;
 public class CardHistory {
     private Date date;//発生日時
     private int price;//金額
-    private int pointBalance;//ポイント残高
-    private int point;//特別なポイントの増減
     private int balance;//処理後の残高
     private String type;//履歴の種類(決済,チャージなど)
     private int typeFlag;//履歴の種類のフラグ（生データ）
-    private String discount;//乗継割引当の情報(Ayuca用)
+    private String discount;//乗継割引の情報(Ayuca用)
     private String device;//処理を行った装置情報
     private String start;//乗車した駅・停留所
     private String end;//降車した駅・停留所
+
     private Boolean pointFlag;//ポイント関係のデータを扱うか否かのフラグ
-    private int additionalPoint;
+    private int pointBalance;//ポイント残高
+    private int grantedNormalPoint;//付与された通常ポイント
+    private int grantedBonusPoint;//付与されたボーナスポイント
+    private int usedPoint;//使用した（還元された）ポイント
 
     public CardHistory() {
         pointFlag = false;
     }
 
-    public CardHistory(Date _date, int _price, int _pointBalance, int _balance, String _type, int _typeFlag, String _discount, String _device, String _start, String _end) {
+    public CardHistory(Date _date, int _price,int _pointBalance, int _balance, String _type, int _typeFlag, String _discount, String _device, String _start, String _end) {
         date = _date;
         price = _price;
         pointBalance = _pointBalance;
@@ -50,48 +52,48 @@ public class CardHistory {
     }
 
 
-    public void setDate(Date _date) {
-        date = _date;
-    }
-
-    public void setPrice(int _price) {
-        price = _price;
-    }
-
-    public void setPointBalance(int _point) {
-        pointBalance = _point;
-    }
-
-    public void setBalance(int _balance) {
-        balance = _balance;
-    }
-
-    public void setType(String _type) {
-        type = _type;
-    }
-
-    public void setDiscount(String _discount) {
-        discount = _discount;
-    }
-
-    public void setDevice(String _device) {
-        device = _device;
-    }
-
-    public void setStart(String _start) {
-        start = _start;
-    }
-
-    public void setEnd(String _end) {
-        end = _end;
-    }
+//    public void setDate(Date _date) {
+//        date = _date;
+//    }
+//    public void setPrice(int _price) {
+//        price = _price;
+//    }
+//
+//    public void setPointBalance(int _point) {
+//        pointBalance = _point;
+//    }
+//
+//    public void setBalance(int _balance) {
+//        balance = _balance;
+//    }
+//
+//    public void setType(String _type) {
+//        type = _type;
+//    }
+//
+//    public void setDiscount(String _discount) {
+//        discount = _discount;
+//    }
+//
+//    public void setDevice(String _device) {
+//        device = _device;
+//    }
+//
+//    public void setStart(String _start) {
+//        start = _start;
+//    }
+//
+//    public void setEnd(String _end) {
+//        end = _end;
+//    }
 
     public void setPointFlag(Boolean _pointFlag) {
         pointFlag = _pointFlag;
     }
-    public void setPointParams(int _point,int _additionalPoint){
-        point = _point;
-        additionalPoint = _additionalPoint;
+    public void setPointParams(int _grantedNormalPoint,int _grantedBonusPoint,int _usedPoint){
+        grantedNormalPoint = _grantedNormalPoint;
+        grantedBonusPoint = _grantedBonusPoint;
+        usedPoint = _usedPoint;
     }
 
     public Date getDate() {
@@ -138,5 +140,14 @@ public class CardHistory {
         return pointFlag;
     }
 
+    public int getGrantedNormalPoint(){
+        return grantedNormalPoint;
+    }
+    public int getGrantedBonusPoint(){
+        return grantedBonusPoint;
+    }
+    public int getUsedPoint(){
+        return usedPoint;
+    }
 
 }
