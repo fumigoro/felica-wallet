fcb3c969731ce6645976e7a82b7aaaad2afbfe39
FUMI <51395778+fumigoro@users.noreply.github.com>
2021-06-27 02:51:27 +0900

    NFCReaderにIFを追加

---
+++ app/src/main/java/com/example/y3033067/nfcreader/NFCReaderIf.java
@@ -0,0 +1,13 @@
+package com.example.y3033067.nfcreader;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public interface NFCReaderIf {
+    public ArrayList<CardHistory> getHistories();
+    public int getSFBalance();
+    public int getPointBalance();
+    public void readAllData();
+    public String getIDm();
+}
